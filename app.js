a513edb68a36c9875cf5524bd64e2e2c858e2c66
Ethan Pavolik <etp12@pitt.edu>
2017-03-09 02:58:27 -0500

    Add cron job to remove old images

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var request = require('request');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var db;
-var keys = require('./keys.js');
+var keys = require('./secrets.js');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+var cron = require('cron')
+var fs = require('fs')
+var util = require('util')
+
 const PORT = 3000;
 
 const headers = {
@@ -12,16 +18,34 @@ const headers = {
     'Content-Type':     'application/x-www-form-urlencoded'
 };
 
+const uploadScanner = cron.job('0 0 * * * *', function() {
+	console.log('Scanning directory..');
 
+	var fileList = fs.readdirSync(__dirname + '/uploads');
+	fileList.forEach(function(filename) {
+		var path = __dirname + '/uploads/' + filename;
+		var stats = fs.statSync(path);
+
+		if (stats) {
+			var mtime = new Date(util.inspect(stats.mtime))
+			var now = new Date();
+			var elapsed = now.getTime() - mtime.getTime();
+			if (elapsed > 600000) fs.unlinkSync(path);
+		}
+	});
+});
+
+uploadScanner.start();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-const mongourl = 'mongodb://' + keys.user +
-	':' + keys.password + '@ds123400.mlab.com:23400/messageboard';
+app.use('/uploads', express.static('uploads'))
+//app.use(bodyParser.urlencoded({extended: true}));
+const mongourl = keys.dburi;
 
 app.get('/', function (req, res) {
 	db.collection('messages').find().toArray(function(err, results) {
 		console.log(results);
-		res.render('index.ejs', {messages:results});
+		const npages = results.length / 10;
+		res.render('index.ejs', {messages:results, pages:npages});
 	});
 });
 
@@ -33,12 +57,12 @@ app.get('/api/messages', function (req, res) {
 	});
 });
 
-app.post('/api/messages', function (req, res) {
-
+app.post('/api/messages', upload.single('file'), function (req, res) {
+	if (req.file) req.body.filepath = req.file.path;
 	db.collection('messages').save(req.body, (err, result) => {
 		if (err) return console.log(err);
 	});
-	console.log(req.body);
+	//console.log('posted: ' + req.body);
 	res.redirect('/');
 });
 
